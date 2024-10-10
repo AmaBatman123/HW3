2e9cab3cdbfe3877e8aa705f2c899bfe121bf3df
Valerii <amabatman.12@gmail.com>
2024-10-10 16:30:32 +0600

    Added moneyX method

---
+++ home/homeWork3.py
@@ -0,0 +1,15 @@
+class Bank():
+
+
+    def __init__(self, name, balance):
+        self._name = name
+        self._balance = balance
+
+    def moneyX(self):
+        sum = input("Enter sum")
+        if sum > 0:
+            self._balance += sum
+        else:
+            print(f"Invalid amount")
+        return print(f"Balance succsessfully updated. You balance now {self._balance}")
+

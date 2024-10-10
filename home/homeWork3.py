f5f462f592d39734df081af16ebef7e537238fba
Valerii <amabatman.12@gmail.com>
2024-10-10 16:36:07 +0600

    Add class instance

--- home/homeWork3.py
+++ home/homeWork3.py
@@ -6,7 +6,7 @@ class Bank():
         self._balance = balance
 
     def moneyX(self):
-        sum = input("Enter sum")
+        sum = int(input())
         if sum > 0:
             self._balance += sum
         else:
@@ -27,3 +27,31 @@ class Bank():
             print(f"Balances merged! New Balance: {self._balance}")
         else:
             print("The object is not a Bank instance. Cannot merge balances.")
+
+# Создание экземпляров
+bank1 = Bank("Bank1", 1000)
+bank2 = Bank("Bank2", 2000)
+
+# Применение метода get_info для вывода начального состояния
+print(bank1._balance)
+print(bank2._balance)
+
+# Применение метода moneyX для добавления денег на счет bank1
+print("\nAdding money to Bank1:")
+bank1.moneyX()
+
+# Применение защищенного метода _kill для обнуления баланса bank2
+print("\nResetting balance of Bank2:")
+bank2._kill()
+print(bank2._balance)
+
+# Применение скрытого метода __jackpot для умножения баланса bank1 на 10
+print("\nApplying jackpot to Bank1:")
+bank1._Bank__jackpot()
+print(bank1._balance)
+
+# Применение защищенного метода _merge_balance для объединения баланса bank2 с bank1
+print("\nMerging balance of Bank2 into Bank1:")
+bank1._merge(bank2)
+print(bank1._balance)
+print(bank2._balance)  # Bank2 remains with 0 balance since it was reset
